a5946171c2a856692d2e3887f826bdac93640bb5
Nina del Rosario <nina.del@gmail.com>
2023-07-05 11:16:15 -0700

    adding notes about functionality from project description

--- script.js
+++ script.js
@@ -1,28 +1,165 @@
-const input = document.querySelector('#fruit');
-const suggestions = document.querySelector('.suggestions ul');
+// # Springboard-Fruit-Search-with-Autocomplete
+// A search UI project from the Springboard Software Engineering bootcamp.
+// ## Requirements
+// When this project is done it will have the following functionality:
+// * On-screen search bar with placeholder text
+// * An event-listener for keystrokes which will trigger the function search()
+// * * The function will filter a list of fruit based on user input
+//   * If the string in the user input appears ANYWHERE in the fruit name, it should be added to results list
+//   * It also should not matter if a user types upper or lower case letters
+// * The autocomplete results will be displayed as a dropdown
+// * On mouse hover, an event listener will highlight the suggested item under the user's cursor
+// * On click, an event listener will trigger a function called useSuggestion(), which will populate the search box with the user's selected suggestiion
+// * A stylesheet will define the search UI with a linear gradient background and a translucent background for the search bar which shows the background below
 
-const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
+const input = document.querySelector("#fruit");
 
+// suggestions will be stored in a UL
+// DOM manipulation will be used to create a dynamic UL of items where items in the fruit list are filtered based on text provided by input
+const suggestions = document.querySelector(".suggestions ul");
+
+const fruit = [
+  "Apple",
+  "Apricot",
+  "Avocado 🥑",
+  "Banana",
+  "Bilberry",
+  "Blackberry",
+  "Blackcurrant",
+  "Blueberry",
+  "Boysenberry",
+  "Currant",
+  "Cherry",
+  "Coconut",
+  "Cranberry",
+  "Cucumber",
+  "Custard apple",
+  "Damson",
+  "Date",
+  "Dragonfruit",
+  "Durian",
+  "Elderberry",
+  "Feijoa",
+  "Fig",
+  "Gooseberry",
+  "Grape",
+  "Raisin",
+  "Grapefruit",
+  "Guava",
+  "Honeyberry",
+  "Huckleberry",
+  "Jabuticaba",
+  "Jackfruit",
+  "Jambul",
+  "Juniper berry",
+  "Kiwifruit",
+  "Kumquat",
+  "Lemon",
+  "Lime",
+  "Loquat",
+  "Longan",
+  "Lychee",
+  "Mango",
+  "Mangosteen",
+  "Marionberry",
+  "Melon",
+  "Cantaloupe",
+  "Honeydew",
+  "Watermelon",
+  "Miracle fruit",
+  "Mulberry",
+  "Nectarine",
+  "Nance",
+  "Olive",
+  "Orange",
+  "Clementine",
+  "Mandarine",
+  "Tangerine",
+  "Papaya",
+  "Passionfruit",
+  "Peach",
+  "Pear",
+  "Persimmon",
+  "Plantain",
+  "Plum",
+  "Pineapple",
+  "Pomegranate",
+  "Pomelo",
+  "Quince",
+  "Raspberry",
+  "Salmonberry",
+  "Rambutan",
+  "Redcurrant",
+  "Salak",
+  "Satsuma",
+  "Soursop",
+  "Star fruit",
+  "Strawberry",
+  "Tamarillo",
+  "Tamarind",
+  "Yuzu",
+];
+
+// * An event-listener for keystrokes which will trigger the function search()
+
+// event listener for dom load?
+// put focus on the search box on page load
+
+// this function handles the logic of searching through the fruit list and returning a list of suggestions
+// this function filters a list of fruit based on user input
+// this function filters list items where the input appears anywhere in the list
+// this function is case insensitive
+// this function is called by searchHandler
 function search(str) {
-	let results = [];
+  let results = [];
+  // TODO
 
-	// TODO
-
-	return results;
+  return results;
 }
 
+// this function is called by the keystroke event listener
 function searchHandler(e) {
-	// TODO
+  // TODO
+  // call the search function with string from input to get array of fruit results
+  // if text input is empty, do not show suggestions
+  // if text put is populated with user typed input
+  // if there are not matches, do not show suggestions
+  // if there is a match from the list, call the showSuggestions function to display suggestions UI
+  // if a suggestion is clicked, do not show suggestions
 }
 
+// this function handles the UI of displaying suggestions below the search box
+// this function is called by the searchHandler function
 function showSuggestions(results, inputVal) {
-
-	// TODO
+  // TODO
+  //
+  // CSS selectors already provided for this project
+  // .search-container {
+  // 	/* TODO */
+  //    }
+  //    .search-container input,
+  //    .search-container .suggestions {
+  //    }
+  //    .search-container input {
+  //    }
+  //    .search-container .suggestions {
+  //    }
+  //    ul {
+  //    }
+  //    ul.has-suggestions {
+  //    }
+  //    ul li {
+  //    }
+  //    ul li:hover {
+  //    }
 }
 
+// this function takes the target of the click and populates the search box with the list item from the target
+// this function is called by the click event listener
 function useSuggestion(e) {
-	// TODO
+  // TODO
 }
 
-input.addEventListener('keyup', searchHandler);
-suggestions.addEventListener('click', useSuggestion);
+// keystrokes will trigger the function searchHandler
+input.addEventListener("keyup", searchHandler);
+suggestions.addEventListener("click", useSuggestion);
