06b96c18195695e48d6a46d1bc43c1e5d3b59521
Nina del Rosario <nina.del@gmail.com>
2023-07-08 12:16:23 -0700

    adding tests

--- script.test.js
+++ script.test.js
@@ -31,5 +31,37 @@ describe("parseArr", function () {
 });
 
 // test search function
+describe("search", function () {
+  it("returns an empty array when there is no match", function () {
+    let testResult = search("77sdfjslkdfj7");
+    expect(testResult).toEqual([]);
+  });
+  it("returns a non-empty array when there is a match", function () {
+    let testResult = search("a");
+    expect(testResult.length).toBeGreaterThan(0);
+  });
+  it("results length is not greater than suggestionDisplayCount", function () {
+    let testResult = search("a");
+    expect(testResult.length).toBeLessThanOrEqual(suggestionDisplayCount);
+  });
+});
 
-// test returnHTML
+// test getDisplayHTML
+describe("getDisplayHTML", function () {
+  it("returns a string of html when there's a space mismatch", function () {
+    let testHTML = getDisplayHTML("heL Lo", 2, "ll");
+    expect(testHTML).toEqual('he<span class="match-text">L L</span>o');
+  });
+  it("returns a string of html when there's a match in the middle", function () {
+    let testHTML = getDisplayHTML("heLLo", 2, "ll");
+    expect(testHTML).toEqual('he<span class="match-text">LL</span>o');
+  });
+  it("returns a string of html when there's a match is at the beginning", function () {
+    let testHTML = getDisplayHTML("heLLo", 2, "HE");
+    expect(testHTML).toEqual('<span class="match-text">he</span>LLo');
+  });
+  it("returns a string of html when there's a match is at the end", function () {
+    let testHTML = getDisplayHTML("heLLo", 2, "llO");
+    expect(testHTML).toEqual('he<span class="match-text">LLo</span>');
+  });
+});
